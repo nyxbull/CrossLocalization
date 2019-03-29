934fd014b5eded4d9b3b68ca152e6cc98577329e
Ilya Lehchylin <i.lehchylin@nyxbull.com>
2019-03-29 16:40:01 +0300

    added localization library for xamarin and windows

---
+++ src/Nyxbull.Plugins.XamLoc/LocalizationException.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Nyxbull.Plugins.XamLoc
+{
+	/// <summary>
+	/// The exception is thrown when an error occurs during package initializing and configuring
+	/// </summary>
+	internal class LocalizationException : Exception
+	{
+		/// <summary>
+		/// Initializes a new instance of the <see cref="T:Nyxbull.Plugins.XamLoc.LocalizationException"/> class
+		/// </summary>
+		/// <param name="message">The error message that explains the reason of the exception</param>
+		public LocalizationException(string message) : base(message) { }
+	}
+}
